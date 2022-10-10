3601cdadbe0f3f252f219cdecdcdb09c0848ec65
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-10-10 10:13:05 -0500

    episodes and location endpoints added

--- rickandmorty/main.py
+++ rickandmorty/main.py
@@ -3,6 +3,8 @@ import requests
 root_url = "https://rickandmortyapi.com/api"
 
 session = requests.Session()
+
+
 def get_all_characters():
     characters = []
     url = f"{root_url}/character"
@@ -26,6 +28,53 @@ def get_all_characters():
     return characters
 
 
-chars = get_all_characters()
-print(len(chars))
+def get_all_locations():
+    locations = []
+    url = f"{root_url}/location"
+    curr_page = 2
+    first_page = session.get(url).json()
+    num_pages = first_page['info']['pages']
 
+    response = requests.get(url)
+    print(response.json())
+
+    for location in response.json()["results"]:
+        locations.append(location["name"])
+        print(location["name"])
+
+    while curr_page <= num_pages:
+        response = session.get(url, params={"page": curr_page})
+        for location in response.json()["results"]:
+            locations.append(location["name"])
+            print(location["name"])
+        curr_page += 1
+    return locations
+
+
+def get_all_episodes():
+    episodes = []
+    url = f"{root_url}/episode"
+    curr_page = 2
+    first_page = session.get(url).json()
+    num_pages = first_page['info']['pages']
+
+    response = requests.get(url)
+    print(response.json())
+
+    for episode in response.json()["results"]:
+        episodes.append(episode["name"])
+        print(episode["name"])
+
+    while curr_page <= num_pages:
+        response = session.get(url, params={"page": curr_page})
+        for episode in response.json()["results"]:
+            episodes.append(episode["name"])
+            print(episode["name"])
+        curr_page += 1
+    return episodes
+
+
+chars = get_all_characters()
+locs = get_all_locations()
+epis = get_all_episodes()
+print(len(chars))
