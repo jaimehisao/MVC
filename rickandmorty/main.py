fd67cce8c4a12e4ed1592a38b2bcbb4f44fe5720
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-10-10 16:10:52 -0500

    Added Location and Episode DTOs

--- rickandmorty/main.py
+++ rickandmorty/main.py
@@ -1,4 +1,7 @@
 import requests
+from dto.character import Character
+from dto.location import Location
+from dto.episode import Episode
 
 root_url = "https://rickandmortyapi.com/api"
 
@@ -13,17 +16,17 @@ def get_all_characters():
     num_pages = first_page['info']['pages']
 
     response = requests.get(url)
-    print(response.json())
 
     for character in response.json()["results"]:
-        characters.append(character["name"])
-        print(character["name"])
+        characters.append(Character(name=character["name"],
+                                    species=character["species"],
+                                    gender=character["gender"],
+                                    number_of_episodes=0))
 
     while curr_page <= num_pages:
         response = session.get(url, params={"page": curr_page})
         for character in response.json()["results"]:
             characters.append(character["name"])
-            print(character["name"])
         curr_page += 1
     return characters
 
@@ -36,17 +39,17 @@ def get_all_locations():
     num_pages = first_page['info']['pages']
 
     response = requests.get(url)
-    print(response.json())
 
     for location in response.json()["results"]:
-        locations.append(location["name"])
-        print(location["name"])
+        locations.append(Location(name=location["name"],
+                                  _type=location["type"],
+                                  dimension=location["dimension"],
+                                  number_of_residents=0))
 
     while curr_page <= num_pages:
         response = session.get(url, params={"page": curr_page})
         for location in response.json()["results"]:
             locations.append(location["name"])
-            print(location["name"])
         curr_page += 1
     return locations
 
@@ -59,17 +62,17 @@ def get_all_episodes():
     num_pages = first_page['info']['pages']
 
     response = requests.get(url)
-    print(response.json())
 
     for episode in response.json()["results"]:
-        episodes.append(episode["name"])
-        print(episode["name"])
+        episodes.append(Episode(name=episode["name"],
+                                air_date=episode["air_date"],
+                                episode_number=episode["episode"],
+                                number_of_characters=0))
 
     while curr_page <= num_pages:
         response = session.get(url, params={"page": curr_page})
         for episode in response.json()["results"]:
             episodes.append(episode["name"])
-            print(episode["name"])
         curr_page += 1
     return episodes
 
@@ -77,4 +80,3 @@ def get_all_episodes():
 chars = get_all_characters()
 locs = get_all_locations()
 epis = get_all_episodes()
-print(len(chars))
