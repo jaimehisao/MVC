23892c0fc3858a331419b985a7631f47db570d0e
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-10-11 23:23:46 -0500

    aki ta

--- rickandmorty2/main.py
+++ rickandmorty2/main.py
@@ -1,28 +1,5 @@
-from fastapi import FastAPI, Depends
-
-from callers import get_all_characters, get_all_episodes, get_all_locations
-from dto.episode import Episode
-from dto.character import Character
-from dto.location import Location
-
+from fastapi import FastAPI
+from view.endpoints import router
 
 app = FastAPI()
-
-locations = get_all_locations()
-episode_dict = get_all_episodes()
-characters = get_all_characters()
-
-
-@app.get("/episodes", response_model=list[Episode])
-def get_episodes() -> list[Episode]:
-    return episode_dict
-
-
-@app.get("/locations", response_model=list[Location])
-def get_locations():
-    return locations
-
-
-@app.get("/characters", response_model=list[Character])
-def get_characters():
-    return characters
+app.include_router(router)
